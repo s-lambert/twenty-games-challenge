0614c85bfb403b5c2c0956b204e3e991d9489c8c
Scott Lambert <s-lambert@live.com.au>
2023-11-10 17:37:57 +0800

    Add ball bouncing off of walls

--- src/pong/mod.rs
+++ src/pong/mod.rs
@@ -6,7 +6,8 @@ pub struct PongPlugin;
 impl Plugin for PongPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::Pong), setup_pong)
-            .add_systems(Update, player_movement);
+            .add_systems(Update, player_movement)
+            .add_systems(Update, ball_movement);
     }
 }
 
@@ -21,7 +22,9 @@ struct Player;
 struct Opponent;
 
 #[derive(Component)]
-struct Ball;
+struct Ball {
+    direction: Vec2,
+}
 
 fn setup_pong(mut commands: Commands) {
     // Player
@@ -58,7 +61,9 @@ fn setup_pong(mut commands: Commands) {
     ));
     // Ball
     commands.spawn((
-        Ball,
+        Ball {
+            direction: Vec2::new(3.0, 2.0).normalize(),
+        },
         SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb(1.0, 1.0, 1.0),
@@ -93,5 +98,36 @@ fn player_movement(
         player_transform.translation.y = (player_transform.translation.y
             - time.delta_seconds() * PADDLE_SPEED)
             .clamp(bottom_boundary, top_boundary);
+    }
+}
+
+fn ball_movement(time: Res<Time>, mut ball_query: Query<(&mut Transform, &mut Ball)>) {
+    let Some((mut ball_transform, mut ball)) = ball_query.iter_mut().next() else {
+        return;
+    };
+
+    let top_boundary = ((WINDOW_WIDTH - PADDLE_WIDTH) / 2.0);
+    let bottom_boundary = top_boundary * -1.0;
+
+    ball_transform.translation.x += time.delta_seconds() * PADDLE_SPEED * ball.direction.x;
+    ball_transform.translation.y += time.delta_seconds() * PADDLE_SPEED * ball.direction.y;
+
+    let x = ball_transform.translation.x;
+    let y = ball_transform.translation.y;
+
+    if x >= top_boundary || x <= bottom_boundary {
+        ball_transform.translation.x = ball_transform
+            .translation
+            .x
+            .clamp(bottom_boundary + 1.0, top_boundary - 1.0);
+        ball.direction.x *= -1.0;
+    }
+
+    if y >= top_boundary || y <= bottom_boundary {
+        ball_transform.translation.y = ball_transform
+            .translation
+            .y
+            .clamp(bottom_boundary + 1.0, top_boundary - 1.0);
+        ball.direction.y *= -1.0;
     }
 }
